fd51a712bbca67c6b4d98f2af076a3f2988a7bc9
shawn.xiao <>
2024-05-07 15:24:04 +0800

    添加了创建工单的功能。在应用运行时，可以通过访问所设置的URL来创建工单并将其提交至数据库。

--- ticket_system/tickets/urls.py
+++ ticket_system/tickets/urls.py
@@ -1,7 +1,8 @@
 # tickets/urls.py
 from django.urls import path
-from .views import ticket_list
+from .views import ticket_list, ticket_create
 
 urlpatterns = [
     path('', ticket_list, name='ticket_list'),
+    path('create/', ticket_create, name='ticket_create'),
 ]
--- ticket_system/tickets/views.py
+++ ticket_system/tickets/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import Ticket
+from django.shortcuts import redirect, render
+from .forms import TicketForm
+
 
 def ticket_list(request):
     tickets = Ticket.objects.all()
     return render(request, 'tickets/ticket_list.html', {'tickets': tickets})
+
+
+def ticket_create(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+    else:
+        form = TicketForm()
+    return render(request, 'tickets/ticket_form.html', {'form': form})
