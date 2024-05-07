d6429c3730dde505bd49704f17ff9e57b0c57f3a
shawn.xiao <>
2024-05-07 16:00:02 +0800

    工单系统中设置用户认证和权限控制。

--- ticket_system/tickets/views.py
+++ ticket_system/tickets/views.py
@@ -5,12 +5,18 @@ from .forms import TicketForm
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Ticket
 from .forms import TicketForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from .forms import TicketForm
+from django.views.generic import View
+
+
 
 def ticket_list(request):
     tickets = Ticket.objects.all()
     return render(request, 'tickets/ticket_list.html', {'tickets': tickets})
 
-
+@login_required
 def ticket_create(request):
     if request.method == 'POST':
         form = TicketForm(request.POST)
@@ -21,7 +27,11 @@ def ticket_create(request):
         form = TicketForm()
     return render(request, 'tickets/ticket_form.html', {'form': form})
 
-
+class TicketEditView(PermissionRequiredMixin, View):
+    model = Ticket
+    form_class = TicketForm
+    permission_required = 'tickets.change_ticket'
+    # ...更多视图方法，如get, post等...
 
 def ticket_edit(request, pk):
     ticket = get_object_or_404(Ticket, pk=pk)
