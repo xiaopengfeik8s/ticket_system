983c9694ec04a5a500e07dc8a035dfad44eec595
shawn.xiao <>
2024-05-07 15:29:39 +0800

    添加编辑和删除工单的能力

--- ticket_system/tickets/urls.py
+++ ticket_system/tickets/urls.py
@@ -1,8 +1,10 @@
 # tickets/urls.py
 from django.urls import path
-from .views import ticket_list, ticket_create
+from .views import ticket_list, ticket_create, ticket_edit, ticket_delete
 
 urlpatterns = [
     path('', ticket_list, name='ticket_list'),
     path('create/', ticket_create, name='ticket_create'),
+    path('<int:pk>/edit/', ticket_edit, name='ticket_edit'),
+    path('<int:pk>/delete/', ticket_delete, name='ticket_delete'),
 ]
--- ticket_system/tickets/views.py
+++ ticket_system/tickets/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Ticket
 from django.shortcuts import redirect, render
 from .forms import TicketForm
-
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Ticket
+from .forms import TicketForm
 
 def ticket_list(request):
     tickets = Ticket.objects.all()
@@ -18,3 +20,21 @@ def ticket_create(request):
     else:
         form = TicketForm()
     return render(request, 'tickets/ticket_form.html', {'form': form})
+
+
+
+def ticket_edit(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    if request.method == 'POST':
+        form = TicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+    else:
+        form = TicketForm(instance=ticket)
+    return render(request, 'tickets/ticket_form.html', {'form': form})
+
+def ticket_delete(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    ticket.delete()
+    return redirect('ticket_list')
